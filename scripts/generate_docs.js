e9c1fcdcb887af023a4e81fb50e11fd22279d600
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-11-03 19:14:13 +0100

    fix: generate_docs script

--- scripts/generate_docs.js
+++ scripts/generate_docs.js
@@ -1,9 +1,11 @@
 
+const { execSync } = require('child_process');
 const { generate } = require('soldoc');
 const path = require('path');
 
 const BASE = path.join([__dirname, '..'].join('/'));
-const INPUT_FILE = [BASE, 'src', 'wisdom-token.sol'].join('/');
+const CONTRACTS_DIR = [BASE, 'src'].join('/');
 const OUTPUT_DIR = [BASE, 'docs'].join('/');
 
-generate('html', [], 'docs/', INPUT_FILE, BASE, 'spec', BASE);
+execSync(`rm -rdf ${OUTPUT_DIR}`);
+generate('html', [], 'docs/', CONTRACTS_DIR, BASE, 'spec', BASE);
